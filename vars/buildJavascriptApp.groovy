4803f6ed57975976ecfa8fef2160f51560679d37
Phani Kumar <pk@Phanis-MacBook-Pro.local>
2020-09-27 23:10:05 +0530

    added another jenkins shared library

---
+++ vars/buildJavascriptApp.groovy
@@ -0,0 +1,25 @@
+def call(Map config=[:], Closure body){
+
+    node{
+        git url : "https://github.com/werne2j/sample-nodejs.git"
+     
+        stage("Install"){
+            sh "npm install"
+        }
+
+        stage("Test"){
+            sh "npm test"
+        }
+
+        stage("Deploy") {
+            if (config.deploy){
+                sh "npm publish" 
+            }
+        }
+        body()
+    }
+
+
+
+
+}
